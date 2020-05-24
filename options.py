5cbcdabc1018c76bff131326a453253e6474d97f
Thane <thane@appvizo.com>
2020-05-24 13:51:42 -0400

    Fixes PUTS and UI.

--- options.py
+++ options.py
@@ -4,6 +4,7 @@ options.py
 --by tkp
 
 An option analysis program.
+PySimpleGUI reference: https://pysimplegui.readthedocs.io/en/latest/
 
 
 """
@@ -23,6 +24,7 @@ def take_closest(myList, myNumber):
     Assumes myList is sorted. Returns closest value to myNumber.
 
     If two numbers are equally close, return the smallest number.
+    WARNING: bisect_left requires the list to be sorted from LOW to HIGH.
     """
     pos = bisect_left(myList, myNumber)
     if pos == 0 or pos == len(myList):
@@ -62,10 +64,16 @@ def textElement(keyval, text='00.0', isInput=True):
     return sg.Input(text=text, key=keyval, text_color='black', font=font_h14, size=size_1, disabled=True)
     #return sg.Text(text=text, key=keyval, text_color='black', background_color='lightblue', size=size_1)
 
+def emptyElement(keyval, isInput=False):
+    #return sg.Input(key=keyval, font=font_h14, size=size_1, text_color='darkblue', background_color='darkblue', visible=True, disabled=True)
+    return sg.Text(key=keyval, font=font_h14, size=size_1, text_color='darkblue', background_color='darkblue', visible=True)
+    
 
 # Column layout
 head = makeHeader(['Strike','Premium','Target 1', 'Gain 1', 'Target 2', 'Gain 2'])
-column = [head, 
+targets = [emptyElement('r0c1'), emptyElement('r0c2'), textElement('__target1', True), emptyElement('r0c4'), textElement('__target2', True), emptyElement('r0c6')]
+column = [targets,
+        head,
         [textElement('r1c1'), textElement('r1c2'), textElement('r1c3'), textElement('r1c4'), textElement('r1c5'), textElement('r1c6')],
         [textElement('r2c1'), textElement('r2c2'), textElement('r2c3'), textElement('r2c4'), textElement('r2c5'), textElement('r2c6')],
         [textElement('r3c1'), textElement('r3c2'), textElement('r3c3'), textElement('r3c4'), textElement('r3c5'), textElement('r3c6')],
@@ -84,10 +92,10 @@ layout = [
     [sg.Text('Price\t', font=font_h14), sg.Input(key='__price', size=(10, 1), font=font_h14),
         sg.Text('Expires\t', font=font_h14), sg.Input(key='__expdate', size=(10, 1), font=font_h14), sg.CalendarButton('Set date',target=(2,3),format='%Y-%m-%d', font=font_h14)],
     [sg.Text('_'  * 80, font=font_h14)],      
-    [sg.Text('Target 1\t', font=font_h14), sg.Input(key='__target1', size=(10, 1), font=font_h14),
-        sg.Text('Target 2\t', font=font_h14), sg.Input(key='__target2', size=(10, 1), font=font_h14)],
-    [sg.Text('Strike #1\t', font=font_h14), sg.Input(key='__strike_price', size=(10, 1), font=font_h14),
-        sg.Text('Strike inc\t', font=font_h14), sg.Input(key='__strike_price_inc', size=(10, 1), font=font_h14)],
+    #[sg.Text('Target 1\t', font=font_h14), sg.Input(key='__target1', size=(10, 1), font=font_h14),
+        #sg.Text('Target 2\t', font=font_h14), sg.Input(key='__target2', size=(10, 1), font=font_h14)],
+    #[sg.Text('Strike #1\t', font=font_h14), sg.Input(key='__strike_price', size=(10, 1), font=font_h14),
+        #sg.Text('Strike inc\t', font=font_h14), sg.Input(key='__strike_price_inc', size=(10, 1), font=font_h14)],
     [sg.Text(' '  * 80, font=font_h14)],
     [sg.Column(column, background_color='darkblue')], #'#d3dfda')],      
     [sg.Text(' '  * 80, font=font_h14)],
@@ -98,12 +106,16 @@ layout = [
 ]
 
 
-def get_strikes(strike1, price, striks, is_call=True, num_options=6):
+def get_strikes(price, striks, is_call=True, num_options=6):
     strikes = list(striks) 
+    print('get_strikes')
+    print(price, is_call, strikes)
+    idx = take_closest(strikes, float(price))
+    print(idx) #, strikes[idx])
     if not is_call:
+        # PUTS
         strikes.reverse()
-    cmpval = price if strike1 == 0 else strike1
-    idx = take_closest(strikes, float(cmpval))
+        idx = len(strikes) - idx
     return strikes[idx:idx+num_options]
 
 
@@ -124,14 +136,15 @@ while True:
         expdate = None if values['__expdate'] == '' else date.fromisoformat(values['__expdate'])
         target1 = 0 if values['__target1'] == '' else Decimal(values['__target1'])
         target2 = 0 if values['__target2'] == '' else Decimal(values['__target2'])
-        strike1 = 0 if values['__strike_price'] == '' else Decimal(values['__strike_price'])
-        strike_inc = 0 if values['__strike_price_inc'] == '' else Decimal(values['__strike_price_inc'])
-        if not is_call:
-            strike_inc *= -1
+        #strike1 = 0 if values['__strike_price'] == '' else Decimal(values['__strike_price'])
+        #strike_inc = 0 if values['__strike_price_inc'] == '' else Decimal(values['__strike_price_inc'])
+        #if not is_call:
+         #   strike_inc *= -1
         if symbol is not None and len(symbol) > 0:
             s = Stock(symbol)
             window['__price'].update(s.price)
         # Get strikes and populate.
+        # TODO: Wait for the price to update
         if expdate is not None:
             o = None # This is our option object.
             if is_call:
@@ -142,34 +155,49 @@ while True:
                 print("PUTS")
             if len(o.strikes) > 0:
                 print("Strikes", o.strikes)
-                strikes = get_strikes(strike1, price, o.strikes, is_call, num_options)
+                strikes = get_strikes(price, o.strikes, is_call, num_options)
                 for i,strik in enumerate(strikes):
                     strike = Decimal(strik)
                     o.set_strike(strike)
                     row = f'r{i+1}'
+                    price = Decimal(o.price)
                     window[row+'c1'].update(o.strike)
                     window[row+'c2'].update(o.price)
                     if target1 > 0:
-                        window[row+'c3'].update(target1)
-                        pctgain = (target1 - strike - Decimal(o.price)) * 100 / Decimal(o.price)
+                        gain = strike - target1 - price
+                        if is_call:
+                            gain = target1 - strike - price
+                        window[row+'c3'].update(f'{gain:.2f}')
+                        pctgain = gain * 100 / price
                         window[row+'c4'].update(f'{pctgain:.2f}%')
                     if target2 > 0:
-                        window[row+'c5'].update(target2)
-                        pctgain = (target2 - strike - Decimal(o.price)) * 100 / Decimal(o.price)
+                        gain = strike - target2 - price
+                        if is_call:
+                            gain = target2 - strike - price
+                        window[row+'c5'].update(f'{gain:.2f}')
+                        pctgain = gain * 100 / price
                         window[row+'c6'].update(f'{pctgain:.2f}%')
     else:
         # Recalc values
+        target1 = 0 if values['__target1'] == '' else Decimal(values['__target1'])
+        target2 = 0 if values['__target2'] == '' else Decimal(values['__target2'])
         for i in range(num_options):
             row = f'r{i+1}'
             strike = Decimal(values[row+'c1'])
             price = Decimal(values[row+'c2'])
-            target1 = Decimal(values[row+'c3'])
             if target1 > 0:
-                pctgain = (target1 - strike - price) * 100 / price
+                gain = strike - target1 - price
+                if is_call:
+                    gain = target1 - strike - price
+                window[row+'c3'].update(f'{gain:.2f}')
+                pctgain = gain * 100 / price
                 window[row+'c4'].update(f'{pctgain:.2f}%')
-            target2 = Decimal(values[row+'c5'])
             if target2 > 0:
-                pctgain = (target2 - strike - price) * 100 / price
+                gain = strike - target2 - price
+                if is_call:
+                    gain = target2 - strike - price
+                window[row+'c5'].update(f'{gain:.2f}')
+                pctgain = gain * 100 / price
                 window[row+'c6'].update(f'{pctgain:.2f}%')
 
 window.close()
